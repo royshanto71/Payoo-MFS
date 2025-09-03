d68d7299de0fe32c784e95d36034b3143e82fe71
Roy Shanto <122017004+royshanto71@users.noreply.github.com>
2025-09-03 18:52:34 +0600

    Refactor input IDs for cashout, transfer, and pay bill forms for clarity and consistency; update toggling feature script accordingly

--- js files/toggling-feature.js
+++ js files/toggling-feature.js
@@ -1,12 +1,14 @@
 //Add Money, Cashout, Transfer Money, Get Bonus, Pay Bill, Transactions toggling feature
 
-const addMoneyBtn = document.getElementById("add-money");
-const cashoutBtn = document.getElementById("cashout");
-const transferBtn = document.getElementById("transfer-balance");
-const getBonusBtn = document.getElementById("get-bonus");
-const payBillBtn = document.getElementById("pay-bill");
-const transactionsBtn = document.getElementById("transactions");
+//get elements
+const addMoney = document.getElementById("add-money");
+const cashout = document.getElementById("cashout");
+const transferBalance = document.getElementById("transfer-balance");
+const getBonus = document.getElementById("get-bonus");
+const payBill = document.getElementById("pay-bill");
+const transactions = document.getElementById("transactions");
 
-addMoneyBtn.addEventListener("click", function () {
-    
-})
+//add event listeners
+addMoney.addEventListener("click", function () {
+    console.log("Add Money clicked");
+});
