ddcb961d1ccaa42705104f998959ec2635a77f84
Roy Shanto <122017004+royshanto71@users.noreply.github.com>
2025-09-03 18:36:47 +0600

    Refactor homepage button IDs for consistency and add toggling feature script

---
+++ js files/toggling-feature.js
@@ -0,0 +1,12 @@
+//Add Money, Cashout, Transfer Money, Get Bonus, Pay Bill, Transactions toggling feature
+
+const addMoneyBtn = document.getElementById("add-money");
+const cashoutBtn = document.getElementById("cashout");
+const transferBtn = document.getElementById("transfer-balance");
+const getBonusBtn = document.getElementById("get-bonus");
+const payBillBtn = document.getElementById("pay-bill");
+const transactionsBtn = document.getElementById("transactions");
+
+addMoneyBtn.addEventListener("click", function () {
+    
+})
